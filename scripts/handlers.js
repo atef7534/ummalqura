b81776be27aa3a5ba5c3d507c3093c4faba501d2
atif_7534 <77503367+atef7534@users.noreply.github.com>
2025-10-31 14:35:14 +0200

    Changing to a smooth design with more functions and more details

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -1,3 +1,10 @@
 function handleBarIcon(target) {
-    
+    const navLinks = target.parentNode.previousElementSibling;
+    if (navLinks.style.visibility !== "hidden") {
+        navLinks.style.visibility = "hidden";
+    }
+    else
+    {
+        navLinks.style.visibility = "visibile";
+    }
 }
